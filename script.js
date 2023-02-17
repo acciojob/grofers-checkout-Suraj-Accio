008df38479318b8a713c2937b6b50e10fd7b3ef8
Suraj-Accio <124239927+Suraj-Accio@users.noreply.github.com>
2023-02-17 16:10:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,21 +6,23 @@ const getSum = () => {
   getSumBtn.disabled = true;
   const prices = document.querySelectorAll(".price");
   let totalPrice = 0;
-  prices.forEach(price => {
+  prices.forEach((price) => {
     const value = parseInt(price.textContent);
-    if(!Number.isNaN(value)){
+    if (!Number.isNaN(value)) {
       totalPrice += value;
     }
   });
   const totalPriceRow = document.createElement("tr");
   totalPriceRow.id = "ans";
   const totalPriceData = document.createElement("td");
+  const totalPriceAns = document.createElement("td");
   totalPriceRow.appendChild(totalPriceData);
-  const data = `Total Price (in Rs): ${totalPrice}`;
+  totalPriceRow.appendChild(totalPriceAns);
+  const data = `Total Price (in Rs): `;
   totalPriceData.append(data);
+  totalPriceAns.append(`${totalPrice}`);
   const table = document.querySelector("tbody");
   table.appendChild(totalPriceRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
